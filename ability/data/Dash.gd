4de9899478131d49ff66cb5cbfeb96318ea0fc9a
Dylan <11531208+Dill-Lan@users.noreply.github.com>
2023-12-24 23:15:19 +1300

    Lock direction when dashing to current direction upon execution

--- ability/data/Dash.gd
+++ ability/data/Dash.gd
@@ -1,18 +1,20 @@
 class_name Dash extends Ability
 
-const _DASH_SPEED_MULTIPLIER = 7
+const _DASH_SPEED_MULTIPLIER = 6
 
-var _dash_time_remaining = 0.0
+var _velocity_snapshot: Vector2 = Vector2(0, 0)
+var _dash_time_remaining: float = 0.0
 
 func _init(_type: Ability.Type, _metadata: AbilityMetadata):
 	super(_type, _metadata)
 
 func execute(player: CharacterBody2D):
+	_velocity_snapshot = player.velocity
 	_dash_time_remaining = 0.2
 	pass
 
 func cycle(player: CharacterBody2D, delta: float):
 	if _dash_time_remaining > 0:
 		_dash_time_remaining -= delta
-		player.velocity = player.velocity.normalized() * Constants.PLAYER_SPEED * _DASH_SPEED_MULTIPLIER
+		player.velocity = _velocity_snapshot.normalized() * Constants.PLAYER_SPEED * _DASH_SPEED_MULTIPLIER
 	pass
